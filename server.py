a00ecdd43c4f4574fc75e6f5e591edc54c7697aa
Ludovit Jonas <lu.jonas99@gmail.com>
2020-06-30 12:24:50 +0200

    favicon updated

--- server.py
+++ server.py
@@ -4,9 +4,9 @@ import csv
 
 app = Flask(__name__)
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static/assets'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
+# @app.route('/favicon.ico')
+# def favicon():
+#     return send_from_directory(os.path.join(app.root_path, 'static/assets'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/')
 def home():
